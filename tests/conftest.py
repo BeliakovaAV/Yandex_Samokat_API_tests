76a7c9526031f5fa9930901cd218d2855819e7df
Alya Beliakova <alyabelya117@gmail.com>
2025-04-23 15:11:28 +0400

    test for order list return created and orders_list_meth added

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from generators import generate_courier_creation_body, generate_order_creation_body
 from methods.courier_login_meth import CourierLoginMeth
 from methods.courier_creation_meth import CourierCreationMeth
+from methods.order_creation_meth import OrderCreationMethod
 
 
 @pytest.fixture
@@ -11,3 +12,12 @@ def generate_courier():
     yield courier_body
     courier_id = CourierLoginMeth.get_courier_id_by_login_password(courier_body["login"], courier_body["password"])
     CourierCreationMeth.delete_courier(courier_id)
+
+
+def generate_order():
+    order_body = generate_order_creation_body()
+    new_order = OrderCreationMethod.create_order(order_body)
+    yield new_order
+    order_track = OrderCreationMethod.get_order_track_number(new_order)
+    OrderCreationMethod.cancel_order(order_track)
+
